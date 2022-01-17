2d40467583e80b94c8e947921c322587464df1ca
naveen4yalla <45757384+naveen4yalla@users.noreply.github.com>
2022-01-16 18:30:46 -0800

    Update models.py

--- teammember/models.py
+++ teammember/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
-# Create your models here.
+
 class People(models.Model):
     firstname = models.CharField(max_length=40,null=False,blank=False)
     lastname =models.CharField(max_length=40,null=True,blank=False)
